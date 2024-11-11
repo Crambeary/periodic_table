8d595ff4a1d1a3e1882edc79dbb1e833101a069c
Crambeary <marctotaro94@gmail.com>
2024-11-11 04:07:59 +0000

    feat: detect input type

--- element.sh
+++ element.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=periodic_table -c --tuples-only"
+PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 fi
 
+# fail if anything other than spaces or numbers
+# fail if reading ERROR or if response is blank
+
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  echo "detected number"
+elif [[ $1 =~ ^[A-Z]$|^[A-Z][a-z]$ ]]; then
+  echo "detected symbol"
+elif [[ $1 =~ ^[a-zA-Z]+$ ]]; then
+  echo "detected name"
+else
+  echo "detected none"
+fi
